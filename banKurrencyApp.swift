165656d335b93b0ec123972ae03d942140c530f0
Miguel <msanchez@izertis.com>
2022-02-21 18:13:39 +0100

    App structure

---
+++ banKurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  banKurrencyApp.swift
+//  banKurrency
+//
+//  Created by miguel on 21/2/22.
+//
+
+import SwiftUI
+
+@main
+struct banKurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
